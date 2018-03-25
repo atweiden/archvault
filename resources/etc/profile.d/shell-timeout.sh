138e663f827b4bac4a6375ad8d82887e2c0d5933
Andy Weidenbaum <archbaum@gmail.com>
2018-03-24 22:32:24 -0400

    convert shell-timeout.sh to bash

--- resources/etc/profile.d/shell-timeout.sh
+++ resources/etc/profile.d/shell-timeout.sh
@@ -1,5 +1,6 @@
-TMOUT="$(( 60*10 ))";
+#!/bin/bash
+TMOUT="$((60 * 10))";
 [[ -z "$DISPLAY" ]] && export TMOUT;
-case $( /usr/bin/tty ) in
+case "$(/usr/bin/tty)" in
   /dev/tty[0-9]*) export TMOUT;;
 esac
